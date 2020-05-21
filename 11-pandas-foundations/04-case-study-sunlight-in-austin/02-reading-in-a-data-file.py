495c65e13edb90c594254e1cf3b3aa43ee4a3142
rizkymamat <65703967+rizkymamat@users.noreply.github.com>
2020-05-21 09:17:09 +0200

    Update for this
    Hi just found that this version isn't new. 
    
    Best
    Rizky

--- 11-pandas-foundations/04-case-study-sunlight-in-austin/02-reading-in-a-data-file.py
+++ 11-pandas-foundations/04-case-study-sunlight-in-austin/02-reading-in-a-data-file.py
@@ -19,10 +19,13 @@ INSTRUCTIONS
 import pandas as pd
 
 # Read in the data file: df
-df = pd.read_csv('data.csv')
+df = pd.read_csv(data_file)
 
 # Print the output of df.head()
 print(df.head())
 
 # Read in the data file with header=None: df_headers
-df_headers = pd.read_csv('data.csv', header=None)
+df_headers = pd.read_csv(data_file, header=None)
+
+# Print the output of df_headers.head()
+print(df_headers.head())
